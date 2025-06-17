e0efd02b7bfc7c43a001457128ed33ce2a408370
Ahmad DAOUD <dodidaoud15@gmail.com>
2025-06-17 15:46:40 +0200

    Removed cors from api and removed unused imports

--- Program.cs
+++ Program.cs
@@ -1,16 +1,7 @@
 using DotNetEnv;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.HttpOverrides;
-using MySqlConnector;
 using RevisiaAPI.Services;
-using System.Data.Common;
 using System.Text;
 
 
@@ -25,22 +16,7 @@ var audience = builder.Configuration["JWT_AUDIENCE"] ?? throw new Exception("JWT
 DbConnection.Init(builder.Configuration);
 
 // Add services
-builder.Services.AddControllers(); builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy =>
-    {
-        policy
-            .WithOrigins(
-                "http://localhost:5173",
-                "http://localhost:4173",
-                "https://revisia.adaoud.dev",
-                "https://apirevisia.adaoud.dev"
-            )
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
-    });
-});
+builder.Services.AddControllers();
 
 
 builder.Services.AddSingleton<JwtService>();
@@ -78,13 +54,6 @@ builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
-    KnownProxies = { System.Net.IPAddress.Parse("127.0.0.1") }
-});
-
-app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
 
