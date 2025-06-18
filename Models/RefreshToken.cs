36bda440b30f3c15352068503fe268b6906e1c76
Ahmad Daoud <115790541+DodiBTW@users.noreply.github.com>
2025-06-18 21:43:01 +0200

    Added automatic Refresh token & stuffs

---
+++ Models/RefreshToken.cs
@@ -0,0 +1,15 @@
+﻿namespace RevisiaAPI.Models
+{
+    public class RefreshToken
+    {
+        public string Token { get; set; } = null!;
+        public string HashedValue { get; set; } = null!;
+        public DateTime ExpiresAt { get; set; }
+        public int UserId { get; set; }
+        public string? LastTokenHash { get; set; }
+        public bool IsInvalidated { get; set; } = false;
+        public DateTime? InvalidatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -7,5 +7,6 @@
         public string PasswordHash { get; set; } = null!;
         public string Email { get; set; } = null!;
         public DateTime? CreatedAt { get; set; }
+        public bool RememberMe { get; set; } = false;
     }
 }
